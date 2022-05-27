22fab17dd5cb2f42f36bc4d0bcb37dcad6e1a4da
chirayu-xx <cboss1436@gmail.com>
2022-05-27 20:02:15 +0530

    links

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,48 +1,61 @@
-import React from 'react'
-import './Portfolio.css'
-import {Swiper,SwiperSlide} from 'swiper/react'
-import CuPedia from '../../img/cupedia.png'
-import Delivery from '../../img/delivery.png'
-import Login from '../../img/login.png'
-import Tracker from '../../img/tracker.png'
-import Spotify from '../../img/spotify.png'
-import'swiper/css'
-import {themeContext} from '../../Context'
+import React from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import CuPedia from "../../img/cupedia.png";
+import Netflix from "../../img/netflix.png";
+import Login from "../../img/login.png";
+import Tracker from "../../img/tracker.png";
+import Spotify from "../../img/spotify.png";
+import "swiper/css";
+import { themeContext } from "../../Context";
 import { useContext } from "react";
-import { Pagination } from 'swiper'
-
+import { Pagination } from "swiper";
 
 export const Portfolio = () => {
-    const theme = useContext(themeContext);
+  const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-  
+
   return (
-    <div className="portfolio" id='Portfolio'>
-        <span style={{color: darkMode? 'white': ''}}>Recent Projects</span>
-        <span>Portfolio</span>
-        <Swiper
+    <div className="portfolio" id="Portfolio">
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
+      <span>Portfolio</span>
+      <Swiper
         autoplay={{ delay: 2000 }}
         modules={[Pagination]}
         slidesPerView={1}
-        pagination={{clickable:true}}
-        className='portfolio-slider'
-        >
-            <SwiperSlide>
-                <img src={CuPedia} alt='img'/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Spotify} alt='img'/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Login} alt='img'/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Tracker} alt='img'/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Delivery} alt='img'/>
-            </SwiperSlide>
-        </Swiper>
+        pagination={{ clickable: true }}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <a
+            href="netflix-clone-three-phi.vercel.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Netflix} alt="img" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={Spotify} alt="img" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={CuPedia} alt="img" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={Login} alt="img" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={Tracker} alt="img" />
+          </a>
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
-}
+  );
+};
